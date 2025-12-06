87c1d93ecbc420054523aabc44b23b1d72898a8f
sathiya <sathiyasankar102@gmail.com>
2025-12-06 21:39:08 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import EventForm from "./Fullstack/React/EventForm";
+import bg from "./assets/bg.jpg"; // <<< Background image import
+
+function App() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundImage: `url(${bg})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backdropFilter:"blur(2px)", // Makes text readable over image
+      padding: "20px"
+    }}>
+      <EventForm />
+    </div>
+  );
+}
